853b3c2a1c7bcaca0c5f99da15fb5f675742a32d
FwParzy <guardianiisaac@gmail.com>
2022-07-02 20:14:57 -0400

    Added Eslint, and started recording inputs to website

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,47 @@
-import './App.css';
+import "./App.css";
+import {useState} from 'react';
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [sex, setSex] = useState("");
+  const [gender, setGender] = useState("");
+
+  const returnInfo = () => {
+    console.log(name + age + sex + gender)
+  };
+
   return (
     <div className="App">
+      <div className="information">
+        <h2>Checklist</h2>
+        <label>Name:</label>
+          <input type="text" 
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+        <label>Age</label>
+          <input type="number" 
+            onChange={(event) => {
+              setAge(event.target.value);
+            }}
+          />
+        <label>Sex</label>
+        <input type="text" 
+            onChange={(event) => {
+              setSex(event.target.value);
+            }}
+        />
+        <label>Gender</label>
+        <input type="text" 
+            onChange={(event) => {
+              setGender(event.target.value);
+            }}
+        />
+        <button onClick={returnInfo}>Add to Database</button>
+      </div>
     </div>
   );
 }
